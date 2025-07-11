1b57d9e89a4dd9d7e356778fdcdb619fa3adbb0a
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-11 19:10:07 +0000

    feature: add controller to fire the grpc setup

---
+++ auth-service/src/main/java/com/poultry/authservice/controller/AuthController.java
@@ -0,0 +1,19 @@
+package com.poultry.authservice.controller;
+
+import com.poultry.authservice.UserResponse;
+import com.poultry.authservice.client.UserGrpcClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class AuthController {
+    private final UserGrpcClient userGrpcClient;
+
+    public UserResponse getUser(@RequestParam String email) {
+        return userGrpcClient.getUserByEmail(email);
+    }
+}
