d87bd6d2941597e60b21fbefb4fb0740b0287b65
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-11 14:58:54 +0000

    feature: add user repository to the user service

---
+++ user-service/src/main/java/com/poultry/userservice/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.poultry.userservice.repository;
+
+import com.poultry.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository  extends JpaRepository<User, Long> {
+}
