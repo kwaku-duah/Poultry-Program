aca096e466b8c1270e112300b7491627d07c8142
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-11 23:27:34 +0000

    feature: add password hashing implementation

--- user-service/src/main/java/com/poultry/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/poultry/userservice/config/SecurityConfig.java
@@ -2,11 +2,27 @@ package com.poultry.userservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/users/**").permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .build();
+    }
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
