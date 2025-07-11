7e98d8cea887b41a9c45ca1236033ebea0ba5eaa
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-11 22:56:55 +0000

    feature: add password encoding before saving to db

--- user-service/src/main/java/com/poultry/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/poultry/userservice/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.poultry.userservice.exception.DuplicateResourceException;
 import com.poultry.userservice.mapper.UserMapper;
 import com.poultry.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,13 +15,18 @@ public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
     @Override
     public void addUser(UserRequest userRequest) {
         if (userRepository.existsByEmail(userRequest.email())) {
             throw new DuplicateResourceException("Email " + userRequest.email() +  " already exists");
         }
+
         User user = userMapper.toEntity(userRequest);
+        String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         userRepository.save(user);
 
     };
