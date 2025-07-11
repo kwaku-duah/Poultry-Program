d21f80ae7f2c949a6e3c82f2e0a2d1a83bcdbea8
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-11 19:54:19 +0000

    feature: add a record dto to return to the frontend, deserializes the proto

--- auth-service/src/main/java/com/poultry/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/poultry/authservice/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.poultry.authservice.controller;
 import com.poultry.authservice.UserResponse;
 import com.poultry.authservice.client.UserGrpcClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final UserGrpcClient userGrpcClient;
 
+    @GetMapping("/auth")
     public UserResponse getUser(@RequestParam String email) {
         return userGrpcClient.getUserByEmail(email);
     }
