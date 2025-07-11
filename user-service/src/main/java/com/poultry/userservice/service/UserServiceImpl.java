d73e53dc0db87e240b1cf70862c8aa467d152ca5
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-11 16:04:13 +0000

    feature: add api response for successful creations

--- user-service/src/main/java/com/poultry/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/poultry/userservice/service/UserServiceImpl.java
@@ -1,12 +1,18 @@
 package com.poultry.userservice.service;
 
 import com.poultry.userservice.entity.User;
+import com.poultry.userservice.mapper.UserMapper;
+import com.poultry.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl extends UserService{
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
     @Override
     public void addUser(User user) {
 
