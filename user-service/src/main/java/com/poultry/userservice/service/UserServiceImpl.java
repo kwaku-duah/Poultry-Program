647931c8ad2b3579b773ceb7edf877c3bbf5dedc
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-11 16:01:20 +0000

    feature: add creating a user logic

---
+++ user-service/src/main/java/com/poultry/userservice/service/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.poultry.userservice.service;
+
+import com.poultry.userservice.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl extends UserService{
+    @Override
+    public void addUser(User user) {
+
+    }
+}
