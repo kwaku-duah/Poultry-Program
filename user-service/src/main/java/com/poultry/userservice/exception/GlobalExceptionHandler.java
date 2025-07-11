53b57b039937c346886c4de29be9abafa2d166ee
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-11 15:37:16 +0000

    feature: add general generic exception handler

--- user-service/src/main/java/com/poultry/userservice/exception/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/poultry/userservice/exception/GlobalExceptionHandler.java
@@ -1,7 +1,17 @@
 package com.poultry.userservice.exception;
 
+import com.poultry.userservice.Payload.GeneralErrorResponseHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<GeneralErrorResponseHandler> errorHandler(Exception ex) {
+       GeneralErrorResponseHandler handler = new GeneralErrorResponseHandler("Something went wrong, please contact suport", HttpStatus.INTERNAL_SERVER_ERROR.value());
+       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(handler);
+    }
 }
