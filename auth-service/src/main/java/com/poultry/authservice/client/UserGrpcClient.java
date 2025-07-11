0ab879a7e5c1d0407755d1d5158d0c6fdeb0db1c
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-11 19:54:19 +0000

    feature: add a record dto to return to the frontend, deserializes the proto

--- auth-service/src/main/java/com/poultry/authservice/client/UserGrpcClient.java
+++ auth-service/src/main/java/com/poultry/authservice/client/UserGrpcClient.java
@@ -3,16 +3,22 @@ package com.poultry.authservice.client;
 import com.poultry.authservice.GetUserByEmailRequest;
 import com.poultry.authservice.UserResponse;
 import com.poultry.authservice.UserServiceGrpc;
-import lombok.RequiredArgsConstructor;
-import net.devh.boot.grpc.client.inject.GrpcClient;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class UserGrpcClient {
 
-    @GrpcClient("user-service")
-    private UserServiceGrpc.UserServiceBlockingStub blockingStub;
+
+    private final UserServiceGrpc.UserServiceBlockingStub blockingStub;
+
+    public UserGrpcClient() {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
+                .usePlaintext()
+                .build();
+        this.blockingStub = UserServiceGrpc.newBlockingStub(channel);
+    }
 
     public UserResponse getUserByEmail(String email) {
         GetUserByEmailRequest request = GetUserByEmailRequest.newBuilder()
---
+++ auth-service/src/main/java/com/poultry/authservice/dto/UserDto.java
@@ -0,0 +1,4 @@
+package com.poultry.authservice.dto;
+
+public record UserDto(Long id, String username, String password) {
+}
