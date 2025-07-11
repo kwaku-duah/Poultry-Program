21cbfbbd15482416f42c6d4b7480db7d9658a075
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-11 15:29:47 +0000

    feature: add implementation of userservice grpc for grpc server

--- user-service/src/main/java/com/poultry/userservice/service/UserServiceServer.java
+++ user-service/src/main/java/com/poultry/userservice/service/UserServiceServer.java
@@ -1,11 +1,16 @@
 package com.poultry.userservice.service;
 
+import com.poultry.userservice.GetUserByEmailRequest;
+import com.poultry.userservice.UserResponse;
+import com.poultry.userservice.UserServiceGrpc;
+import com.poultry.userservice.entity.User;
 import com.poultry.userservice.repository.UserRepository;
+import io.grpc.stub.StreamObserver;
 import org.springframework.grpc.server.service.GrpcService;
 import org.springframework.stereotype.Service;
 
 @GrpcService
-public class UserServiceServer {
+public class UserServiceServer extends UserServiceGrpc.UserServiceImplBase {
 
     private final UserRepository userRepository;
 
@@ -13,5 +18,18 @@ public class UserServiceServer {
         this.userRepository = userRepository;
     }
 
+    @Override
+    public void getUserByEmail(GetUserByEmailRequest request, StreamObserver<UserResponse> responseObserver) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(()-> new RuntimeException("Email not found"));
+
+        UserResponse response = UserResponse.newBuilder()
+                .setId(user.getId())
+                .setEmail(user.getEmail())
+                .setPassword(user.getPassword())
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 
 }
