56fb159fb9da658142da124331fff2c9aa8d772b
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-11 15:07:38 +0000

    feature: add injection of user repository in the service

--- user-service/src/main/java/com/poultry/userservice/service/UserServiceServer.java
+++ user-service/src/main/java/com/poultry/userservice/service/UserServiceServer.java
@@ -1,4 +1,17 @@
 package com.poultry.userservice.service;
 
+import com.poultry.userservice.repository.UserRepository;
+import org.springframework.grpc.server.service.GrpcService;
+import org.springframework.stereotype.Service;
+
+@GrpcService
 public class UserServiceServer {
+
+    private final UserRepository userRepository;
+
+    public UserServiceServer(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
 }
