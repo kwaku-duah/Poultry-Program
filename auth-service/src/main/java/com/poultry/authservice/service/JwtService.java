60e8551f7330b863f64c97ace7039a155fc237ec
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-12 21:03:21 +0000

    feature: add a method interface and implementation to abstract away token generation logic in controller

--- auth-service/src/main/java/com/poultry/authservice/service/JwtService.java
+++ auth-service/src/main/java/com/poultry/authservice/service/JwtService.java
@@ -1,8 +1,11 @@
 package com.poultry.authservice.service;
 
 import com.poultry.authservice.UserResponse;
+import com.poultry.authservice.dto.LoginRequest;
+import com.poultry.authservice.dto.LoginResponseDto;
 
 public interface JwtService {
     String generateAccessToken(UserResponse user);
     String generateRefreshToken(UserResponse user);
+    LoginResponseDto fullUser(LoginRequest loginRequest);
 }
