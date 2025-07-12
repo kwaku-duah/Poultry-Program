9098fb3b1569f4542a92f63a442d7b9eba99ab75
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-12 02:07:57 +0000

    feature: add path exemption in http security

--- auth-service/src/main/java/com/poultry/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/com/poultry/authservice/config/SecurityConfig.java
@@ -2,10 +2,25 @@ package com.poultry.authservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated()
+
+                )
+                .build();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
