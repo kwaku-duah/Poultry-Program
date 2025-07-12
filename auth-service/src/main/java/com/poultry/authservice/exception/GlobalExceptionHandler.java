417b09a89f2f0011c5f489ceb69f49a47085df72
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-12 21:31:35 +0000

    feature: add global error handler for all unseen errors

--- auth-service/src/main/java/com/poultry/authservice/exception/GlobalExceptionHandler.java
+++ auth-service/src/main/java/com/poultry/authservice/exception/GlobalExceptionHandler.java
@@ -1,9 +1,18 @@
 package com.poultry.authservice.exception;
 
+import com.poultry.authservice.payload.GenericErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<GenericErrorResponse> handleGenericError(Exception ex) {
+        GenericErrorResponse response = new GenericErrorResponse("Something went wrong, please contact support", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
