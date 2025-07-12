a05963c84d2e8d1e17be8628c25e53daaac99650
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-12 00:03:52 +0000

    feature: add role based access to the application

---
+++ user-service/src/main/java/com/poultry/userservice/entity/Role.java
@@ -0,0 +1,11 @@
+package com.poultry.userservice.entity;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_VET,
+    ROLE_FARMER,
+    ROLE_SUPERVISOR,
+    ROLE_USER,
+    ROLE_SUPPLIER,
+    ROLE_TRADER
+}
