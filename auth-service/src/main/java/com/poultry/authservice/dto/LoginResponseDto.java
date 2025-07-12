faaa6d7c1b5fbb8555ffc2b734d52a2eac4defc8
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-12 23:47:55 +0000

    refactoring: add removal of password field from frontend response

--- auth-service/src/main/java/com/poultry/authservice/dto/LoginResponseDto.java
+++ auth-service/src/main/java/com/poultry/authservice/dto/LoginResponseDto.java
@@ -1,6 +1,8 @@
 package com.poultry.authservice.dto;
 
+import com.poultry.authservice.Role;
+
 import java.util.Set;
 
-public record LoginResponseDto(Long id, String email, String password, Set<com.poultry.authservice.Role> roles, String accessToken, String refreshToken) {
+public record LoginResponseDto(Long id, String fullName, String email, Set<Role> roles, String accessToken, String refreshToken) {
 }
