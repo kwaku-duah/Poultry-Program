acc87a46ad69140869606df726223f7e8ef4ef5d
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-12 00:27:08 +0000

    feature: add setting of users with default roles

--- user-service/src/main/java/com/poultry/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/poultry/userservice/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.poultry.userservice.service;
 
 import com.poultry.userservice.dto.UserRequest;
+import com.poultry.userservice.entity.Role;
 import com.poultry.userservice.entity.User;
 import com.poultry.userservice.exception.DuplicateResourceException;
 import com.poultry.userservice.mapper.UserMapper;
@@ -8,6 +9,8 @@ import com.poultry.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.EnumSet;
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +30,10 @@ public class UserServiceImpl implements UserService{
         User user = userMapper.toEntity(userRequest);
         String encodedPassword = bCryptPasswordEncoder.encode(userRequest.password());
         user.setPassword(encodedPassword);
+        /*all new users added to the system assume role ROLE_USER, until
+        upgrade, to protect the application against injection
+        * */
+        user.setRoles(EnumSet.of(Role.ROLE_USER));
         userRepository.save(user);
 
     };
