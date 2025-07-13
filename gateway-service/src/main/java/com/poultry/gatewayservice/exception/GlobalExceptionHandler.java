81e99d250e39524a4f03c822637a4d8468547769
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-13 16:01:39 +0000

    feature: add global generic error for unhandled errors

--- gateway-service/src/main/java/com/poultry/gatewayservice/exception/GlobalExceptionHandler.java
+++ gateway-service/src/main/java/com/poultry/gatewayservice/exception/GlobalExceptionHandler.java
@@ -1,11 +1,19 @@
 package com.poultry.gatewayservice.exception;
 
 
+import com.poultry.gatewayservice.payload.GenericErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(value = {Exception.class})
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<GenericErrorResponse> errorHandler(Exception exception) {
+        GenericErrorResponse response = new GenericErrorResponse("Something went wrong, contact suport", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
