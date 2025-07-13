da90423edc26fc44ebfcc0cc4c484a7cb78b4e49
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-13 14:33:37 +0000

    feature: add method to extract claims from payload

--- auth-service/src/main/java/com/poultry/authservice/service/JwtService.java
+++ auth-service/src/main/java/com/poultry/authservice/service/JwtService.java
@@ -5,5 +5,8 @@ import com.poultry.authservice.UserResponse;
 public interface JwtService {
     String generateAccessToken(UserResponse user);
     String generateRefreshToken(UserResponse user);
+    boolean isValidToken(String token);
+    String extractUserId(String token);
+    String extractRole(String token);
 
 }
