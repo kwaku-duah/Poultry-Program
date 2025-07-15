370a9381496d9ab2b03135a375ecd2ee256f47b4
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-15 11:36:55 +0000

    feature: add coop request and response dto

---
+++ farmer-service/src/main/java/com/poultry/farmerservice/mapper/CoopMapper.java
@@ -0,0 +1,17 @@
+package com.poultry.farmerservice.mapper;
+
+import com.poultry.farmerservice.dto.CoopRequestDto;
+import com.poultry.farmerservice.dto.CoopResponseDto;
+import com.poultry.farmerservice.entity.Coop;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CoopMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "farmer", ignore = true)
+    Coop toEntity(CoopRequestDto coopRequestDto);
+
+    CoopResponseDto toCoopResponseDto(Coop coop);
+}
