21a99e8b7013412838ac96a7c6dd026a28e54948
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-15 13:44:33 +0000

    feature: add filtering of all requests at the gateway

--- farmer-service/src/main/java/com/poultry/farmerservice/config/SecurityConfig.java
+++ farmer-service/src/main/java/com/poultry/farmerservice/config/SecurityConfig.java
@@ -1,23 +1,26 @@
 package com.poultry.farmerservice.config;
 
+import com.poultry.farmerservice.security.HeaderAuthenticationFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, HeaderAuthenticationFilter  headerAuthenticationFilter) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/farmers/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(headerAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
