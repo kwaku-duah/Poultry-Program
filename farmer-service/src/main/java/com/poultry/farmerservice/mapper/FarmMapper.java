223b064570177b652f9bb8c3dd9d9e49b752d3dc
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-15 11:04:24 +0000

    feature: add mapper to convert dto

---
+++ farmer-service/src/main/java/com/poultry/farmerservice/mapper/FarmMapper.java
@@ -0,0 +1,18 @@
+package com.poultry.farmerservice.mapper;
+
+
+import com.poultry.farmerservice.dto.FarmRequestDto;
+import com.poultry.farmerservice.entity.Farmer;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface FarmMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "farmerId", ignore = true)
+    @Mapping(target = "coops", expression = "java(new ArrayList<>())")
+    Farmer toEntity(FarmRequestDto farmRequestDto);
+
+
+}
