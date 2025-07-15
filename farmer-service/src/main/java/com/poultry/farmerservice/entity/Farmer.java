fb9580e8b6110a522c947b3dc07e630de559a1fe
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-15 09:44:57 +0000

    feature: add farmer entity

--- farmer-service/src/main/java/com/poultry/farmerservice/entity/Farmer.java
+++ farmer-service/src/main/java/com/poultry/farmerservice/entity/Farmer.java
@@ -1,4 +1,33 @@
 package com.poultry.farmerservice.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "farmers")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Farmer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String farmerId;
+
+    private String farmName;
+
+    private String phoneNumber;
+
+    private String location;
+
+    private String GPSAddress;
+
+    private List<Coop> coops;
 }
