899776f76d78924f3f2b8813b65cf1072e4701e1
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-15 12:09:50 +0000

    feature: add farmer creation request

--- farmer-service/src/main/java/com/poultry/farmerservice/repository/FarmRepository.java
+++ farmer-service/src/main/java/com/poultry/farmerservice/repository/FarmRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface FarmRepository extends JpaRepository<Farmer,Long> {
+    boolean existsByFarmerId(String farmerId);
     Optional<Farmer> findByFarmerId(String farmerId);
 }
