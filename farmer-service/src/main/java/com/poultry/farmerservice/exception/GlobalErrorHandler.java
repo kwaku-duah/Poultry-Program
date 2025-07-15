5f9bc2e105b6ef1d746ae8e28731d73a2ef655d4
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-15 12:15:53 +0000

    feature: add resource not found exception

--- farmer-service/src/main/java/com/poultry/farmerservice/exception/GlobalErrorHandler.java
+++ farmer-service/src/main/java/com/poultry/farmerservice/exception/GlobalErrorHandler.java
@@ -1,6 +1,7 @@
 package com.poultry.farmerservice.exception;
 
 import com.poultry.farmerservice.payload.GeneralErrorResponse;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,5 +20,11 @@ public class GlobalErrorHandler {
     public ResponseEntity<GeneralErrorResponse> duplicateErrorHandler(DuplicateResourceException ex) {
         GeneralErrorResponse response = new GeneralErrorResponse(ex.getMessage(), HttpStatus.CONFLICT.value());
         return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<GeneralErrorResponse> notFoundErrorHandler(ResourceNotFoundException ex) {
+        GeneralErrorResponse response = new GeneralErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
