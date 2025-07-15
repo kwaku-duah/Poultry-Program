66c209f4d2ee525e225f4b5ed3868e39651ee0bf
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-15 11:52:39 +0000

    feature: add global exception handler

--- farmer-service/src/main/java/com/poultry/farmerservice/exception/GlobalErrorHandler.java
+++ farmer-service/src/main/java/com/poultry/farmerservice/exception/GlobalErrorHandler.java
@@ -1,7 +1,17 @@
 package com.poultry.farmerservice.exception;
 
+import com.poultry.farmerservice.payload.GeneralErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalErrorHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<GeneralErrorResponse> genericErrorHandler(Exception ex) {
+        GeneralErrorResponse response = new GeneralErrorResponse("Something went wrong, contact support", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
