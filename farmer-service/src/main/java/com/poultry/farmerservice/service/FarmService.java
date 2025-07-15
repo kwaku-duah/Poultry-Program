78f14a01029c78321d6b355d81f4bb6c36a2192c
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-15 11:43:35 +0000

    feature: add farmers service interface

--- farmer-service/src/main/java/com/poultry/farmerservice/service/FarmService.java
+++ farmer-service/src/main/java/com/poultry/farmerservice/service/FarmService.java
@@ -1,5 +1,13 @@
 package com.poultry.farmerservice.service;
 
-public interface FarmService {
+import com.poultry.farmerservice.dto.FarmRequestDto;
+import com.poultry.farmerservice.dto.FarmResponseDto;
 
+import java.util.List;
+
+public interface FarmService {
+    void createFarmer(String farmerId, FarmRequestDto farmRequestDto);
+    List<FarmResponseDto> getAllFarmers();
+    void updateFarmer(String farmerId, FarmRequestDto farmRequestDto);
+    void deleteFarmer(String farmerId);
 }
