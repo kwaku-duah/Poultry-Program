66d2492e14d898100158a80914f9e9f04ed80ca9
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-15 14:15:59 +0000

    feature: add registration of farmer service to gateway

--- farmer-service/src/main/java/com/poultry/farmerservice/dto/FarmResponseDto.java
+++ farmer-service/src/main/java/com/poultry/farmerservice/dto/FarmResponseDto.java
@@ -1,11 +1,14 @@
 package com.poultry.farmerservice.dto;
 
+import java.util.List;
+
 public record FarmResponseDto(
         String farmerId,
         String farmName,
         String phoneNumber,
         String location,
-        String gpsAddress
+        String gpsAddress,
+        List<CoopResponseDto> coops
 
 ) {
 }
