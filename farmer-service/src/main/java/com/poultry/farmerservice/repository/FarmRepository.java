c7f5fd377df73efb45e198024c5037e9579447a2
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-15 11:54:14 +0000

    feature: add custom method to find by farmerId

--- farmer-service/src/main/java/com/poultry/farmerservice/repository/FarmRepository.java
+++ farmer-service/src/main/java/com/poultry/farmerservice/repository/FarmRepository.java
@@ -3,5 +3,8 @@ package com.poultry.farmerservice.repository;
 import com.poultry.farmerservice.entity.Farmer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FarmRepository extends JpaRepository<Farmer,Long> {
+    Optional<Farmer> findByFarmerId(String farmerId);
 }
