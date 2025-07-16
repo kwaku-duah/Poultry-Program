48b78259e99a6f5f853a1055f16e350f0cbdb59a
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-16 12:20:30 +0000

    feature: add schedule record repo

---
+++ farmer-service/src/main/java/com/poultry/farmerservice/repository/ScheduleRecordRepository.java
@@ -0,0 +1,12 @@
+package com.poultry.farmerservice.repository;
+
+import com.poultry.farmerservice.entity.coupactivity.ScheduleRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ScheduleRecordRepository extends JpaRepository<ScheduleRecord, Long> {
+    List<ScheduleRecord> findByCoop_IdAndCoop_Farmer_FarmerId(Long coopId, String farmerId);
+    Optional<ScheduleRecord> findByIdAndCoop_IdAndCoop_Farmer_FarmerId(Long id, Long coopId, String farmerId);
+}
