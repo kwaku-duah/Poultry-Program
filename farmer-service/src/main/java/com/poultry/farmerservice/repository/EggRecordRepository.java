a0fe7b46dfb2e6ca528bb420b01e315450fa0f46
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-16 11:16:35 +0000

    feature: add egg service db and repository

---
+++ farmer-service/src/main/java/com/poultry/farmerservice/repository/EggRecordRepository.java
@@ -0,0 +1,13 @@
+package com.poultry.farmerservice.repository;
+
+import com.poultry.farmerservice.entity.coupactivity.EggsRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EggRecordRepository extends JpaRepository<EggsRecord, Long> {
+
+    List<EggsRecord> findByCoop_IdAndCoop_Farmer_FarmerId(Long coopId, String farmerId);
+    Optional<EggsRecord> findByIdAndCoop_IdAndCoop_Farmer_FarmerId(Long id, Long coopId, String farmerId);
+}
