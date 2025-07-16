4b06b9dc0fc557b27eb0e1ba45b746972bd1571b
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-16 10:06:23 +0000

    feature: add eggrecord dto mapper

--- farmer-service/src/main/java/com/poultry/farmerservice/dto/coopactivitydto/MortalityResponseDto.java
+++ farmer-service/src/main/java/com/poultry/farmerservice/dto/coopactivitydto/MortalityResponseDto.java
@@ -3,7 +3,7 @@ package com.poultry.farmerservice.dto.coopactivitydto;
 import java.time.LocalDateTime;
 
 public record MortalityResponseDto(
-        LocalDateTime datetime,
+        LocalDateTime date,
         Long coopId,
         Integer numberOfDeaths,
         String reason,
