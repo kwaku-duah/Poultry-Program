332de9adae6e5b7ae68d214ee3a4ba5173df8afc
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-16 13:45:22 +0000

    feature: add vaccine controller

---
+++ farmer-service/src/main/java/com/poultry/farmerservice/controller/activitycontroller/VaccineController.java
@@ -0,0 +1,56 @@
+package com.poultry.farmerservice.controller.activitycontroller;
+
+
+import com.poultry.farmerservice.dto.coopactivitydto.VaccineRequestDto;
+import com.poultry.farmerservice.dto.coopactivitydto.VaccineResponseDto;
+import com.poultry.farmerservice.service.coopactivity.VaccineService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/vaccines")
+@RequiredArgsConstructor
+public class VaccineController {
+
+    private final VaccineService vaccineService;
+
+    @PreAuthorize("hasRole('USER')")
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestHeader("X-Id") String farmerId,
+                                       @Valid @RequestBody VaccineRequestDto dto) {
+        vaccineService.addVaccineRecord(farmerId, dto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/{coopId}")
+    public ResponseEntity<List<VaccineResponseDto>> getAll(@RequestHeader("X-Id") String farmerId,
+                                                           @PathVariable Long coopId) {
+        return ResponseEntity.ok(vaccineService.getVaccineRecords(coopId, farmerId));
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @PutMapping("/{id}/{coopId}")
+    public ResponseEntity<Void> update(@RequestHeader("X-Id") String farmerId,
+                                       @PathVariable Long id,
+                                       @PathVariable Long coopId,
+                                       @Valid @RequestBody VaccineRequestDto dto) {
+        vaccineService.updateVaccineRecord(id, coopId, farmerId, dto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @DeleteMapping("/{id}/{coopId}")
+    public ResponseEntity<Void> delete(@RequestHeader("X-Id") String farmerId,
+                                       @PathVariable Long id,
+                                       @PathVariable Long coopId) {
+        vaccineService.deleteVaccineRecord(id, coopId, farmerId);
+        return ResponseEntity.noContent().build();
+    }
+}
+
