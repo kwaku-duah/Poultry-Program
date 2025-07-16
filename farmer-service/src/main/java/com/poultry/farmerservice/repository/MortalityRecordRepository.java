25bf2b97d1d20d39c75bd7699c62435c8ed910f3
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-16 12:19:48 +0000

    feature: add mortality record repo

---
+++ farmer-service/src/main/java/com/poultry/farmerservice/repository/MortalityRecordRepository.java
@@ -0,0 +1,12 @@
+package com.poultry.farmerservice.repository;
+
+import com.poultry.farmerservice.entity.coupactivity.MortalityRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MortalityRecordRepository extends JpaRepository<MortalityRecord, Long> {
+    List<MortalityRecord> findByCoop_IdAndCoop_Farmer_FarmerId(Long coopId, String farmerId);
+    Optional<MortalityRecord> findByIdAndCoop_IdAndCoop_Farmer_FarmerId(Long id, Long coopId, String farmerId);
+}
