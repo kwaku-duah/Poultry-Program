562b7af7cc8af63057217b80224c9dea6c24361c
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-16 11:16:35 +0000

    feature: add egg service db and repository

---
+++ farmer-service/src/main/java/com/poultry/farmerservice/service/coopactivity/EggServiceImpl.java
@@ -0,0 +1,42 @@
+package com.poultry.farmerservice.service.coopactivity;
+
+import com.poultry.farmerservice.dto.coopactivitydto.EggRequestDto;
+import com.poultry.farmerservice.dto.coopactivitydto.EggResponseDto;
+import com.poultry.farmerservice.mapper.coopactivitymapper.CoopActivityMapper;
+import com.poultry.farmerservice.repository.CoopRepository;
+import com.poultry.farmerservice.repository.EggRecordRepository;
+import com.poultry.farmerservice.repository.FarmRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EggServiceImpl implements EggService {
+
+    private final EggRecordRepository eggRecordRepository;
+    private final FarmRepository farmRepository;
+    private final CoopRepository coopRepository;
+    private final CoopActivityMapper mapper;
+
+    @Override
+    public void addEggRecord(String farmerId, EggRequestDto eggRequestDto) {
+
+    }
+
+    @Override
+    public List<EggResponseDto> getEggRecords(Long coopId, String farmerId) {
+        return List.of();
+    }
+
+    @Override
+    public void updateEggRecord(Long id, Long coopId, String farmerId, EggRequestDto eggRequestDto) {
+
+    }
+
+    @Override
+    public void deleteEggRecord(Long id, Long coopId, String farmerId) {
+
+    }
+}
