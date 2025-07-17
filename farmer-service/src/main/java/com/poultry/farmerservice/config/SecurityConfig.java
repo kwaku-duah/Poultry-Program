51380bfb68f04a34e292230bd251cac68d6ee4c0
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-17 00:29:57 +0000

    feature: add cors configuration for localhost reaact to attach

--- farmer-service/src/main/java/com/poultry/farmerservice/config/SecurityConfig.java
+++ farmer-service/src/main/java/com/poultry/farmerservice/config/SecurityConfig.java
@@ -1,12 +1,10 @@
 package com.poultry.farmerservice.config;
 
 import com.poultry.farmerservice.security.HeaderAuthenticationFilter;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -20,7 +18,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator").permitAll()
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(headerAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
---
+++ gateway-service/src/main/java/com/poultry/gatewayservice/config/CorsGlobalConfig.java
@@ -0,0 +1,28 @@
+package com.poultry.gatewayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+@Configuration
+public class CorsGlobalConfig {
+    
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+         CorsConfiguration config = new CorsConfiguration();
+
+       
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
