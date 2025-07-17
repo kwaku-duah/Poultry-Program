7049c2b9d0be7ab08de64932df8d16cf7ef733b4
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-17 12:17:36 +0000

    feature: add upgrade for a farmer

--- user-service/src/main/java/com/poultry/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/poultry/userservice/service/UserServiceImpl.java
@@ -4,11 +4,13 @@ import com.poultry.userservice.dto.UserRequest;
 import com.poultry.userservice.entity.Role;
 import com.poultry.userservice.entity.User;
 import com.poultry.userservice.exception.DuplicateResourceException;
+import com.poultry.userservice.exception.ResourceNotFoundException;
 import com.poultry.userservice.mapper.UserMapper;
 import com.poultry.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.EnumSet;
 
@@ -21,6 +23,7 @@ public class UserServiceImpl implements UserService{
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 
+    @Transactional
     @Override
     public void addUser(UserRequest userRequest) {
         if (userRepository.existsByEmail(userRequest.email())) {
@@ -39,10 +42,14 @@ public class UserServiceImpl implements UserService{
 
     }
 
+    @Transactional
     @Override
-    public void addFarmer() {
+    public void addFarmer(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User with id " + userId + " not found"));
 
+        if(!user.getRoles().contains(Role.ROLE_FARMER)) {
+            user.setRoles(EnumSet.of(Role.ROLE_FARMER));
+            userRepository.save(user);
+        }
     }
-
-    ;
 }
