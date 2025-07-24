d05cf6c22190926ba9e6c27baa086eafaa1fa09d
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-24 10:07:41 +0000

    feature: add chat respons for ai integration

---
+++ farmer-service/src/main/java/com/poultry/farmerservice/payload/ChatResponse.java
@@ -0,0 +1,15 @@
+package com.poultry.farmerservice.payload;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ChatResponse {
+    private String reply;
+}
