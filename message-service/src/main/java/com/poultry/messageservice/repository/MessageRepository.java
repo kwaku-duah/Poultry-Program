5ece60bbc9ec1c135fefa5d5d37f2b23d562ffaf
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-25 20:13:04 +0000

    feature: add method to find messages belonging to a particular sender and receiver

--- message-service/src/main/java/com/poultry/messageservice/repository/MessageRepository.java
+++ message-service/src/main/java/com/poultry/messageservice/repository/MessageRepository.java
@@ -3,7 +3,11 @@ package com.poultry.messageservice.repository;
 import com.poultry.messageservice.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface MessageRepository extends JpaRepository<Message, UUID> {
+    List<Message> findBySenderIdAndReceiverIdOrReceiverIdAndSenderId ( String senderId, String receiverId,
+                                                                       String receiverId2, String senderId2
+    );
 }
