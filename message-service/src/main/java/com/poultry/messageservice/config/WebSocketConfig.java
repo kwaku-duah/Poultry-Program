f72a405960712f6d73a71c8690bbcb01e67be1f6
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-26 15:57:25 +0000

    feature: add web socket merged with kafka?

---
+++ message-service/src/main/java/com/poultry/messageservice/config/WebSocketConfig.java
@@ -0,0 +1,32 @@
+package com.poultry.messageservice.config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poultry.messageservice.websocket.WebSocketMessageHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new WebSocketMessageHandler(kafkaTemplate, objectMapper), "/ws").setAllowedOrigins("*");
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
