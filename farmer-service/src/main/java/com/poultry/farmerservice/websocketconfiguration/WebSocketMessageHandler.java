357a932ad0d6acca3c4197b2f542c5bd2229599c
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-29 18:25:47 +0000

    feature: add websocket and kafka functionality to the farmerservice

---
+++ farmer-service/src/main/java/com/poultry/farmerservice/websocketconfiguration/WebSocketMessageHandler.java
@@ -0,0 +1,37 @@
+package com.poultry.farmerservice.websocketconfiguration;
+
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.kafka.core.KafkaTemplate;
+import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@RequiredArgsConstructor
+public class WebSocketMessageHandler extends TextWebSocketHandler {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+    @Getter
+    private static final List<WebSocketSession> webSocketSessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        webSocketSessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        kafkaTemplate.send("message-topic", message.getPayload());
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) {
+        webSocketSessions.remove(session);
+    }
+
+}
